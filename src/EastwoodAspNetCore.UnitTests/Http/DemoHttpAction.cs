fd607cd3af8f309097276433d8ceeb5945f12542
Luke Puplett <lukepuplett@hotmail.com>
2017-10-29 13:09:36 +0000

    Adding everything.

---
+++ src/EastwoodAspNetCore.UnitTests/Http/DemoHttpAction.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Eastwood.Http
+{
+    internal class GetFakeDataHttpAction : HttpAction
+    {
+        public GetFakeDataHttpAction()
+            : base(new HttpActionInitializationContext()) { }
+
+        public GetFakeDataHttpAction(HttpActionInitializationContext init)
+            : base(init) { }
+
+        public int DefaultStatusCode { get; set; } = 204;
+
+        public bool WasLocalConditionCalled { get; private set; }
+
+        protected override Task<(bool, IPreconditionInformation)> TryGetLocalConditionInfoAsync(ActionContext actionContext, CancellationToken cancellationToken)
+        {
+            this.WasLocalConditionCalled = true;
+
+            return base.TryGetLocalConditionInfoAsync(actionContext, cancellationToken);
+        }
+
+        protected override Task ExecuteActionAsync(ActionContext actionContext, CancellationToken cancellationToken)
+        {
+            return actionContext.ExecuteWithAsync(this.DefaultStatusCode);
+        }
+    }
+}
